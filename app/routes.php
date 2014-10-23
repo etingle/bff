6081969e06b33124185a39ecf9ed7c2ee38a836b
Evan Tingle <evan.tingle@gmail.com>
2014-10-23 17:24:48 -0400

    readme

--- app/routes.php
+++ app/routes.php
@@ -13,10 +13,12 @@
 
 Route::get('/', function()
 {
-	//return View::make('hello');
+
 	return View::make('home');
 
 });
+
+
 
 Route::get('/lorem-ipsum',function()
 {
--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -3,9 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-    <!--@section('head')
-        <link rel="stylesheet" href="style.css" />
-    @show-->
     {{ HTML::style('http://yui.yahooapis.com/pure/0.5.0/pure-min.css'); }}
 	{{ HTML::style('http://purecss.io/css/layouts/side-menu.css'); }}
     {{ HTML::style('styles/p3.css'); }}
